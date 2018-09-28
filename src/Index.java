0da93ff39a11aa6d6888743b7947386fed3c0546
Mark Heijnekamp <26150@ma-web.nl>
2018-09-28 14:12:41 +0200

    Initial commit

---
+++ src/Index.java
@@ -0,0 +1,14 @@
+public class Index {
+    public static void main (String[] args){
+        boolean ja = false;
+        if (ja == true){
+            System.out.println("This is false");
+        }
+        if (ja == false){
+            System.out.println("This is true");
+        }
+        System.out.println("Hello World");
+
+    }
+
+}
