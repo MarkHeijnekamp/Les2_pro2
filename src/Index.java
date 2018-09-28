5ed6adf4b40ac772a4a9b4b46c7b8260ae314311
Mark Heijnekamp <26150@ma-web.nl>
2018-09-28 14:45:24 +0200

    Input ,Scanner ,Eastereggs

--- src/Index.java
+++ src/Index.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
+
 public class Index {
+
     public static void main (String[] args){
         boolean ja = false;
         if (ja == true){
@@ -7,9 +11,17 @@ public class Index {
         if (ja == false){
             System.out.println("This is true");
         }
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int input = reader.nextInt(); // Scans the next token of the input as an int.
+        //once finished
+        reader.close();
 
-
-        System.out.println("Hello World");
+        System.out.println("you pressed " + input);
+        System.out.println();
+        if (input == 666){
+            System.out.println("YOU DEVIL");
+        }
 
     }
 
